905d09b6919a92367450b7e2c511189f7016273b
Sami <sami.joutsijoki@cs.tamk.fi>
2014-04-07 11:19:35 +0300

    parempi juttu
    kk

--- Engine.js
+++ Engine.js
@@ -2,9 +2,15 @@ var background = new Image();
 background.src = "big_pic.jpg";
 
 var mainCharacterImage = new Image();
-mainCharacterImage.src = "mario.jpg";
+mainCharacterImage.src = "mario.png";
+
+var houseImage = new Image();
+houseImage.src = "house.png";
 
 var mainChar = new gameObject(400, 400, mainCharacterImage);
+var house = new gameObject(500, 500, houseImage);
+var house2 = new gameObject(800, 500, houseImage);
+var house3 = new gameObject(1200, 500, houseImage);
 
 var startX = 50;
 var startY = 50;
@@ -37,9 +43,6 @@ function move(event){
 	var xPos = event.pageX;
 	var yPos = event.pageY;
 
-	var d = document.getElementById("debug");
-	d.innerHTML = xPos + "  " + yPos;
-
 	clearInterval(moveTimer);
 	moveTimer = 0;
 	if (!moveTimer){
@@ -53,42 +56,72 @@ function move(event){
 var dirX;
 var dirY;
 var amount = 0.00001;
+var previousX;
+var previousY;
 function moveMainChar(x, y){
-
-	mainChar.centerX = mainChar.centerX + (x - mainChar.centerX) * amount;
-	mainChar.centerY = mainChar.centerY + (y - mainChar.centerY) * amount;
+	previousX = mainChar.x;
+	previousY = mainChar.y;
+	mainChar.x = mainChar.x + (x - mainChar.x) * amount;
+	mainChar.y = mainChar.y + (y - mainChar.y) * amount;
 	amount += 0.00001;
-	if (amount > 0.5){
-		amount = 0.00001;
+	if (amount > 0.01){
+		amount = 0.01;
 	}
 
-	var d = document.getElementById("debug");
-	d.innerHTML = "MainChar X: " + mainChar.centerX + " MainChar Y: " + mainChar.centerY;
+	//var d = document.getElementById("debug");
+	//d.innerHTML = "MainChar X: " + mainChar.centerX + " MainChar Y: " + mainChar.centerY;
 	redraw();
 }
 
+var bgOffsetX;
+var bgOffsetY;
 function redraw(){
 	ctx.clearRect(0, 0, width, height);
 
-	var bgOffsetX = mainChar.centerX - 400;
-	var bgOffsetY = mainChar.centerY - 400;
+	bgOffsetX = mainChar.x - 400;
+	bgOffsetY = mainChar.y - 400;
 
 	bgOffsetX < 0 ? bgOffsetX = 0 : bgOffsetX = bgOffsetX;
 	bgOffsetY < 0 ? bgOffsetY = 0 : bgOffsetY = bgOffsetY;
 
 	ctx.drawImage(background, bgOffsetX, bgOffsetY, width, height, 0, 0, width, height);
 
+	ctx.drawImage(house.image,
+                  house.x - bgOffsetX,
+                  house.y - bgOffsetY);
+
+	ctx.drawImage(house2.image,
+                  house2.x - bgOffsetX,
+                  house2.y - bgOffsetY);
+
+	ctx.drawImage(house3.image,
+                  house3.x - bgOffsetX,
+                  house3.y - bgOffsetY);
+
 	ctx.drawImage(mainChar.image,
-                  mainChar.centerX,
-                  mainChar.centerY);
+                  mainChar.x,
+                  mainChar.y);
+
+	if (checkCollision(mainChar, house)
+		|| checkCollision(mainChar, house2)
+		|| checkCollision(mainChar, house3)){
+		mainChar.x = previousX;
+		mainChar.y = previousY;
+		//var d = document.getElementById("debug");
+		//d.innerHTML = "Char X: " + mainChar.x + " Char y: " + mainChar.y + " Hero width: " + mainChar.image.width;
+	}
+
+	if (checkCollision(mainChar, house)){
+		house.image.src="mario.png";
+	}
 }
 
-function checkCollision(img1, img2){
+function checkCollision(hero, object){
 
-    if ((img1.x + img1.width) > img2.x 
-      && img1.x < (img2.x + img2.width)
-      && (img1.y + img1.width) > img2.y
-      && img1.y < (img2.y + img2.height)){
+    if ((hero.x + hero.image.width) > object.x - bgOffsetX
+         && hero.x < ((object.x - bgOffsetX) + object.image.width)
+         && (hero.y + hero.image.width) > object.y - bgOffsetY
+         && hero.y < ((object.y - bgOffsetY) + object.image.height)){
       return true;
     } else {
       return false;
