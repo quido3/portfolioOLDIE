fbc5c2a3db97dfcdea7aeca376986351fe4f58cc
Sami <sami.joutsijoki@cs.tamk.fi>
2014-05-05 17:16:56 +0300

    Tehtiin progressia
    nyt oikee branch

--- Engine.js
+++ Engine.js
@@ -1,10 +1,21 @@
 var background = new Image();
-background.src = "big_pic.jpg";
+background.src = "grass.png";
 
 var mainCharacterImage = new Image();
-mainCharacterImage.src = "mario.jpg";
+mainCharacterImage.src = "mario.png";
+
+var mill = new Image();
+mill.src = "mylly.png";
+var millDoor = new Image();
+millDoor.src = "mylly_ovi.png";
+
+var talo = new Image();
+talo.src = "talo.png";
+var taloOvi = new Image();
+taloOvi.src = "talo_ovi.png";
 
 var mainChar = new gameObject(400, 400, mainCharacterImage);
+var objectContainer = new Array();
 
 var startX = 50;
 var startY = 50;
@@ -12,7 +23,24 @@ var startY = 50;
 var c;
 var ctx;
 
+var pause = false;
+
 background.onload=function(){
+
+	objectContainer[0] = new gameObject(500
+									    , 0
+									    , mill
+									    , millDoor
+									    , (mill.width / 2) - (millDoor.width / 2)
+									    , mill.height - millDoor.height);
+
+	objectContainer[1] = new gameObject(500
+										, 750
+										, talo
+										, taloOvi
+										, (talo.width / 2) - (taloOvi.width / 2)
+										, talo.height - taloOvi.height);
+
 	c = document.getElementById("viewport");
 	ctx = c.getContext("2d");
     width = window.innerWidth;
@@ -21,6 +49,7 @@ background.onload=function(){
     document.getElementsByTagName("canvas")[0].setAttribute("height", height);
     ctx.drawImage(background, startX, startY, width, height, 0, 0, width, height);
     ctx.drawImage(mainCharacterImage, mainChar.x, mainChar.y);
+    redraw();
 }
 
 window.onresize=function(){
@@ -34,11 +63,8 @@ window.onresize=function(){
 
 var moveTimer;
 function move(event){
-	var xPos = event.clientX;
-	var yPos = event.clientY;
-
-	var d = document.getElementById("debug");
-	d.innerHTML = xPos + "  " + yPos;
+	var xPos = event.pageX;
+	var yPos = event.pageY;
 
 	clearInterval(moveTimer);
 	moveTimer = 0;
@@ -52,89 +78,140 @@ function move(event){
 
 var dirX;
 var dirY;
+var amount = 0.01;
+var previousX;
+var previousY;
+var intervalX;
+var intervalY;
 function moveMainChar(x, y){
-	var yPerX = getRelativeValue(x, y);
-	
-	var d = document.getElementById("debug");
-	d.innerHTML = yPerX;
+	if (!pause){
+		previousX = mainChar.x;
+		previousY = mainChar.y;
 
-	if (mainChar.x < x){
-		dirX = 1;
-	} else if (mainChar.x > x){
-		dirX = -1;
-	} else {
-		dirX = 0;
-	}
+		mainChar.x += (x - mainChar.x) * amount;
+		mainChar.y += (y - mainChar.y) * amount;
 
-	if (mainChar.y < y){
-		dirY = yPerX * 1;
-	} else if (mainChar.y > y){
-		dirY = yPerX * -1;
-	} else {
-		dirY = 0;
-	}
-
-	if (dirX > 1){
-		dirX = 1;
-	}
-
-	if (dirX < -1){
-		dirX = -1;
-	}
-
-	if (dirY > 1){
-		dirY = 1;
-	}
-
-	if (dirY < -1){
-		dirY = -1;
-	}
-
-	mainChar.x += dirX;
-	mainChar.y += dirY;
-
-	redraw();
-}
-
-function getRelativeValue(x, y){
-
-	if (mainChar.x > x && mainChar.y > y){
-		return ((mainChar.y - y) / (mainChar.x - x));
-	} else if (mainChar.x > x && mainChar.y < y){
-		return ((y - mainChar.y) / (mainChar.x - x));
-	} else if (mainChar.x < x && mainChar.y > y){
-		return ((mainChar.y - y) / (x - mainChar.x));
-	} else if (mainChar.x < x && mainChar.y < y){
-		return ((y - mainChar.y) / (x - mainChar.x));
-	} else {
-		return 1;
+		redraw(x, y);
 	}
 }
 
-function redraw(){
+var bgOffsetX = 0;
+var bgOffsetY = 0;
+var scrollThreshold = 200;
+function redraw(x, y){
 	ctx.clearRect(0, 0, width, height);
 
-	var bgOffsetX = mainChar.x - 400;
-	var bgOffsetY = mainChar.y - 400;
+	scrollBackground();
 
 	bgOffsetX < 0 ? bgOffsetX = 0 : bgOffsetX = bgOffsetX;
 	bgOffsetY < 0 ? bgOffsetY = 0 : bgOffsetY = bgOffsetY;
 
 	ctx.drawImage(background, bgOffsetX, bgOffsetY, width, height, 0, 0, width, height);
 
+	for (var i = 0; i < objectContainer.length; i++){
+		var temp = objectContainer[i];
+		drawObjectImage(temp);
+		drawObjectHitbox(temp);
+
+		if (checkHitboxCollision(mainChar, temp)){
+			// DO SOMETHING	
+		}
+
+		ctx.beginPath();
+		ctx.moveTo(temp.bBoxX - bgOffsetX,
+				   temp.bBoxY - bgOffsetY);
+		ctx.lineTo(temp.bBoxX + temp.bBoxWidth - bgOffsetX,
+				   temp.bBoxY - bgOffsetY);
+		ctx.lineTo(temp.bBoxX + temp.bBoxWidth - bgOffsetX,
+				   temp.bBoxY + temp.bBoxHeight - bgOffsetY);
+		ctx.lineTo(temp.bBoxX - bgOffsetX,
+				   temp.bBoxY + temp.bBoxHeight - bgOffsetY);
+		ctx.lineTo(temp.bBoxX - bgOffsetX,
+				   temp.bBoxY - bgOffsetY);
+		ctx.stroke();
+	}
+
 	ctx.drawImage(mainChar.image,
                   mainChar.x,
                   mainChar.y);
+
+	for (var i = 0; i < objectContainer.length; i++){
+		if (checkCollision(mainChar, objectContainer[i])){
+			mainChar.x = previousX;
+			mainChar.y = previousY;
+		}
+	}
 }
 
-function checkCollision(img1, img2){
+function scrollBackground(){
+	if ((mainChar.x + mainChar.width) > (width - scrollThreshold)){
+		bgOffsetX++;
+	} else if (mainChar.x < scrollThreshold){
+		bgOffsetX--;
+	}
 
-    if ((img1.x + img1.width) > img2.x 
-      && img1.x < (img2.x + img2.width)
-      && (img1.y + img1.width) > img2.y
-      && img1.y < (img2.y + img2.height)){
+	if ((mainChar.y + mainChar.height) > (height - scrollThreshold)){
+		bgOffsetY++;
+	} else if (mainChar.y < scrollThreshold) {
+		bgOffsetY--;
+	}
+}
+
+function drawObjectImage(object){
+	ctx.drawImage(object.image, 
+				  object.x - bgOffsetX,
+				  object.y - bgOffsetY);
+}
+
+function drawObjectHitbox(object){
+	ctx.drawImage(object.hitbox,
+				  object.hitboxX - bgOffsetX,
+				  object.hitboxY - bgOffsetY);
+}
+
+function checkCollision(hero, object){
+
+    if ((hero.x + hero.image.width) > object.x - bgOffsetX
+         && hero.x < ((object.x - bgOffsetX) + object.image.width)
+         && (hero.y + hero.image.width) > object.y - bgOffsetY
+         && hero.y < ((object.y - bgOffsetY) + object.image.height)){
+      	return true;
+    } else {
+      return false;
+    }
+}
+
+function checkHitboxCollision(hero, object){
+
+	if ((hero.x + hero.image.width) > (object.bBoxX - bgOffsetX)
+         && hero.x < ((object.bBoxX - bgOffsetX) + object.bBoxWidth)
+         && (hero.y + hero.image.width) > object.bBoxY - bgOffsetY
+         && hero.y < ((object.bBoxY - bgOffsetY) + object.bBoxHeight)){
       return true;
     } else {
       return false;
     }
+}
+
+function checkClickOnObjects(event) {
+    for (var i = 0; i < objectContainer.length; i++) {
+        var object = objectContainer[i];
+
+        if (checkHitboxCollision(mainChar, object)){
+	        if ((event.pageX) > object.x - bgOffsetX
+		         && event.pageX < ((object.x - bgOffsetX) + object.image.width)
+		         && (event.pageY) > object.y - bgOffsetY
+		         && event.pageY < ((object.y - bgOffsetY) + object.image.height)) {
+	        	alert(object.image.src);
+	            document.getElementById('firstContent').style.visibility = 'visible';
+	            pause = true;
+	        }
+	    }
+    }
+}
+
+function hideContent(event) {
+    var source = event.target || event.srcElement;
+    source.parentNode.parentNode.style.visibility = 'hidden';
+    pause = false;
 }
--- GameObject.js
+++ GameObject.js
@@ -1,8 +1,21 @@
-function gameObject(x, y, img){
+function gameObject(x, y, img, hitboxImg, hX, hY){
 	this.image = img;
 	this.x = x;
 	this.y = y;
+	this.centerX = x + (img.width / 2);
+	this.centerY = y + (img.height / 2);
 	this.width = img.width;
 	this.height = img.height;
+
+	if (hitboxImg){
+		this.hitbox = hitboxImg;
+		this.hitboxX = x + hX;							// Kuvan vasen reuna
+		this.hitboxY = y + hY;							// Kuvan alareuna
+		this.bBoxX = x + hX - (hitboxImg.width / 2);    // Hitboxin vasen reuna
+		this.bBoxY = y + hY;							// Hitboxin alareuna
+		this.bBoxWidth = hitboxImg.width*2;				// Hitboxin leveys
+		this.bBoxHeight = hitboxImg.height*1.5;			// Hitboxin korkeus
+	}
+
 	return this;
 }
